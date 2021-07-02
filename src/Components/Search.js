2e56539002e8bcfaff2088819d68128d94de4297
vaishnavi H <vaishnavi@accuknox.com>
2021-07-02 10:54:20 +0530

    multiselect dropdown

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,116 +1,118 @@
 import React, { useState } from "react";
 import './Search.css';
 
-
-
-const Search=function(props){
-const [name,setName]=useState("",[]);
+function Search(){
+const [name,setName]=useState('');
 const[show,setShow]=useState(false);
-const[addname,setAddName]=useState([]);
+const[addName,setAddName]=useState([]);
 const[filtername,setFilterName]=useState("",[]);
-//const[filterlist,setFilterList]=useState([]);
+//const[remove,setRemove]=useState([]);
+const[hide,setHide]=useState([]);
 
 
-
-
-
-const handleClick =(e)=> {
-  e.preventDefault();
+const handleClick = (e) => {
+e.preventDefault();
  setName(e.target.value);
  AddTask();
- 
 }
-const AddTask =() => {
 
+const AddTask = () => {
+if(name !== ""){
 const taskDetails={
-        
-          value: name,
-    };
-    setAddName([...addname,taskDetails]);
-  
-};    
+         
+          value:name
+};
+    setAddName([...addName,taskDetails]);}
+};
+
+const removeall=()=>{
+  if(name!==""){
+    const clear={
+    value:<button class="btn3" onClick={removeHandle}><i id="fig1" class="fa fa-close"></i></button>,}
+    setHide([clear]);
+   }
+}    
+
+const removeHandle=()=>{
+  setAddName([]);
+  setHide([]);
+}
 
 const removeTask = (e, value)=>{
   e.preventDefault();
-   setAddName(addname.filter((t) => t.value !== value));
+   setAddName(addName.filter((t) => t.value !== value));
 };
 
 const filterHandle =(e)=>{
 setFilterName(e.target.value);
 console.log(filtername);
-
 }
 
-  
-  const options =[
-     { id:1,
-       value:"line3",
+const[options,setOptions]=useState(
+[
+     { 
+       id:1,
+       value:"line1",
      },
-     {id:2,
+     {
+       id:2,
       value:"group",
      },
-     {id:3,
-      value:"line",
+     {
+      id:3,
+      value:"line2",
       },
-     {id:4,
+     {
+      id:4,
       value:"group2",
       },
-      {id:5,
+      {
+        id:5,
        value:"group3",
       }, 
-      {id:6,
+      {
+        id:6,
        value:"group4",
       },
-      {id:7,
-       value:"line2",
+      {
+        id:7,
+       value:"line3",
       },
-   ];
+]);
   
-//if(filtername === options.value){
-  //const details={
-    //data:filtername,
-  //};
-//setFilterList(details);
-//}
-//};
+console.log(options);
 
 const filtered = options.filter((options) =>
-    options.value.toLowerCase().includes(filtername.toLowerCase())
-  );
+options.value.toLowerCase().includes(filtername.toLowerCase())
+);
+
+const removeOption= (e) =>{
+const removeitem =  (options.filter((options) => options.value !==name));
+setOptions(removeitem);
+}
+  
 return(
 <div>
-   
- <div class="box">
- {addname.map((t) => (
-             <li class="ui">{t.value} &nbsp;
-             <button class="btn2" onClick={(e)=> removeTask(e, t.value)}><i class="fa fa-close"></i></button>
-             </li>))}
-             </div>
-             <input type="text" placeholder="search" onChange={(e) => filterHandle(e)}/>
-             <button class="b2"onClick={()=>setShow(!show)} ><i class="fa fa-angle-down"></i></button>
-  
-                 {/* <div>
-                 {filterlist.map((a) => (
-                 <li class="">{a.data} &nbsp;
-               
-                </li>))}
-                </div>*/}
-
-
-               
-             
- 
-{show?
-    
-<div class="container">
-      {filtered.map((a) => (
-       <div class="ba"><option onClick={handleClick}>{a.value}</option></div>
-      ))}
+      <span>{hide.map((r)=>(
+           <div>{r.value} </div>))} 
+      </span>
+<div class="box">
+    {addName.map((t) => (
+        <div class="ui" key={t.id}>{t.value} &nbsp;
+            <i onClick={(e)=> {removeTask(e, t.value);}} id="fig2"class="fa fa-close"></i>
+        </div>))
+    } 
+    <input type="text" placeholder="search" onChange={(e) => filterHandle(e)} /> 
+    <button class="b2"onClick={()=>setShow(!show)} ><i id="fig"class="fa fa-angle-down"></i></button>
+</div> 
+  {show?
+    <div class="container">
+        {filtered.map((a) => (
+          <div class="ba">
+            <option onClick={(e)=> {handleClick(e);removeOption(e);removeall()}}  key={a.id}>{a.value}</option> </div> ))}
+         </div>
+  :null }
 </div>
-      
-:null }
-</div>
-
 );
 }
 export default Search;
